0b245b5f790672a2814ee01b24fb12ba131e33d7
Zyziek055 <sylgermroz@gmail.com>
2025-04-10 22:18:43 +0200

    game.jsx answers input functionality added

--- src/Game.jsx
+++ src/Game.jsx
@@ -6,22 +6,68 @@ import { io } from 'socket.io-client';
 export function Game({ gameId, nickname, selectedCategories }) {
   const [randomLetter, setRandomLetter] = useState('');
   const [showLetterScreen, setShowLetterScreen] = useState(true); // controls when to hide display letter
+  const [selectedCategory, setSelectedCategory] = useState(selectedCategories[0]);
+  const [answers, setAnswers] = useState(
+    selectedCategories.reduce((acc, category) => {
+      acc[category] = '';
+      return acc;
+    }, {})
+  );
+
+  
+
   //TODO: add functionality to show letter screen for 3 seconds and then hide it
   //TODO: functioanlity
+  const handleAnswerChange = (e, category) => {
+    setAnswers(prev => ({
+      ...prev,
+      [category]: e.target.value
+    }));
+  };
+
+  const categoryOnClick = (category) => {
+    setSelectedCategory(category);
+  };
+
     return (
-        <div className="game">
-           {/* show letter screen while shoLetterScreen is true */}
-              <div>
-                <h1>Game Time!</h1>
-                <div style={{ display: 'flex', gap: '10px' }}>
-                  {selectedCategories.map((category) => (
-                    <button key={category} onClick={() => console.log(category)}>
-                      {category}
-                    </button>
-                  ))}
-                </div>
-              </div>
+      <div className="game">
+        <div>
+          <div style={{ display: 'flex', gap: '10px' }}>
+            {selectedCategories.map((category) => (
+              <button 
+                key={category} 
+                onClick={() => categoryOnClick(category)}
+                style={{
+                  backgroundColor: selectedCategory === category ? 'green' : 'black'
+                }}
+              >
+                {category}
+              </button>
+            ))}
+          </div>
+          
+          {/* Input field that appears when category is selected */}
+          {selectedCategory && (
+            <div style={{ marginTop: '20px' }}>
+              <label>
+                {selectedCategory}: 
+                <input
+                  type="text"
+                  value={answers[selectedCategory]}
+                  onChange={(e) => handleAnswerChange(e, selectedCategory)}
+                  placeholder={`Enter ${selectedCategory}`}
+                  style={{ marginLeft: '10px' }}
+                />
+              </label>
+            </div>
+
+          )}
         </div>
+
+        <button>
+          Submit answers
+        </button>
+      </div>
     );
   }
 
